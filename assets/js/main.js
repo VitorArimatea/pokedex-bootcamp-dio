c31ef1db31566834d4a1580b87e27bf207b6f50e
Vitor Arimatea <vitorodev@outlook.com>
2023-08-28 11:12:06 -0300

    new: Primeira forma de buscar e receber o json dos objetos que fiz a requisição

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,20 @@
-console.log('Olá Mundo!')
+const offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+fetch(url)
+  .then(function (response) {
+    
+    response.json().then(function (responseBody) {
+      console.log(responseBody);
+    })
+
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {
+    console.log('Requisição concluída');
+  })
+
+  
