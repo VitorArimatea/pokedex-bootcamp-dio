6cc3e150c286bd98cb81881e23b6c936f846481e
Vitor Arimatea <vitorodev@outlook.com>
2023-08-29 11:21:51 -0300

    new: Criando um arquivo para separar o consumo da API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,32 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+function pokemonToHtml(pokemon){
+  return `
+    <li class="pokemon-card">
+      <div class="pokemon-details">
+        <h2 class="pokemon-name">${pokemon.name}</h2>
+        <span class="pokemon-number">#001</span>
+      </div>
+      <div class="pokemon-details">
+        <ol class="types">
+          <li class="type-item">Grass</li>
+          <li class="type-item">Poison</li>
+        </ol>
+        <img class="pokemon-image" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Uma artwork do ${pokemon.name}">
+      </div>
+    </li>
+  `
+} 
 
-fetch(url)
-  .then((response) => response.json())
-  .then((jsonBody) => jsonBody.results)
-  .then((pokemonList) => {
-    debugger
-    console.log(pokemonList);
-  })
-  .catch((error) => console.log(error))
+const pokemonsList = (document.getElementById('pokemonsList'));
+
+pokeApi.getPokemons().then((pokemonList) => {
+  const listItems = []
+
+  for (let i = 0; i < pokemonList.length; i++) {
+    const pokemon = pokemonList[i];
+    listItems.push(pokemonToHtml(pokemon));
+  }
+
+  console.log(listItems)
+});
 
   
---
+++ assets/js/poke-api.js
@@ -0,0 +1,12 @@
+const pokeApi = {
+  
+}
+
+pokeApi.getPokemons = async (offset = 0, limit = 15) => {
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+  
+  return fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .catch((error) => console.error(error))
+}
