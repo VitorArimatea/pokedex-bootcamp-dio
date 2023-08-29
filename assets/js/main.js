f4ad47e6bb83d124a67ca0a825cc29c52ddb5766
Vitor Arimatea <vitorodev@outlook.com>
2023-08-29 20:06:31 -0300

    new: Criando função para manipular o types dos pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,29 @@
+function pokemonTypesToLi(pokemonTypes) {
+  return pokemonTypes.map((typeSlot) => `<li class="type-item">${typeSlot.type.name}</li>`)
+}
+
 function pokemonToHtml(pokemon){
   return `
     <li class="pokemon-card">
       <div class="pokemon-details">
         <h2 class="pokemon-name">${pokemon.name}</h2>
-        <span class="pokemon-number">#001</span>
+        <span class="pokemon-number">${pokemon.id}</span>
       </div>
       <div class="pokemon-details">
         <ol class="types">
-          <li class="type-item">Grass</li>
-          <li class="type-item">Poison</li>
+          ${pokemonTypesToLi(pokemon.types).join('')}
         </ol>
-        <img class="pokemon-image" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Uma artwork do ${pokemon.name}">
+        <img class="pokemon-image" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png" alt="Uma artwork do ${pokemon.name}">
       </div>
     </li>
   `
 } 
 
-const pokemonsList = (document.getElementById('pokemonsList'))
+const pokemonList = (document.getElementById('pokemonList'))
 
 pokeApi.getPokemons().then((pokemons = []) => {
-  pokemonsList.innerHTML += pokemons.map(pokemonToHtml).join('')
+  const newHtml = pokemons.map(pokemonToHtml).join('');
+  pokemonList.innerHTML = newHtml;
 })
 
   
