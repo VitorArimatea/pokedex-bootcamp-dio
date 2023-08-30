97ca76e9cc39384ba16faa43a435fab1bd271654
Vitor Arimatea <vitorodev@outlook.com>
2023-08-30 10:49:30 -0300

    update: Finalizando o padrão dos detalhes dos pokémons

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,10 +1,26 @@
 const pokeApi = {}
 
-pokeApi.getPokemonDetail = async (pokemon) => {
-  return fetch(pokemon.url).then((response) => response.json())  
+function pokedexDetails(pokeDetail) {
+  const pokemon = new Pokedex()
+  pokemon.name = pokeDetail.name
+  pokemon.number = pokeDetail.id
+
+  const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+  const [type] = types
+
+  pokemon.types = types
+  pokemon.type = type
+
+  return pokemon
 }
 
-pokeApi.getPokemons = async (offset = 0, limit = 100) => {
+pokeApi.getPokemonDetail = async (pokemon) => {
+  return fetch(pokemon.url)
+          .then((response) => response.json())
+          .then(pokedexDetails)
+}
+
+pokeApi.getPokemons = async (offset = 0, limit = 5) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
   
   return fetch(url)
